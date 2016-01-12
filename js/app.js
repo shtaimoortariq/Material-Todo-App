efda8499e489b75080a12e05f3d9cea9d95bd177
taimoor123 <taimoor_t@yahoo.com>
2016-01-12 15:28:42 +0500

    Reaming Functionality is working

--- js/app.js
+++ js/app.js
@@ -2,51 +2,61 @@
  * Created by Taimoor tariq on 1/2/2016.
  */
 
+(function () {
+    angular.module("myApp", ["ngMaterial", "ngMdIcons"])
 
-var app = angular.module("myApp", ["ngMaterial", "ngMdIcons"]);
+        .config(function ($mdThemingProvider) {
+            $mdThemingProvider.theme('default')
+                .primaryPalette('blue')
+                .accentPalette('red');
+        })
+
+        .controller("toDoAppController", [toDoAppController]);
+
+    function toDoAppController() {
+        var _self = this;
+
+        this.myTask = "";
+        this.allTasks = [];
+        this.allTaskLength = 0;
+        this.completedTask = [];
+        this.completedTaskLength = 0;
+        this.removeIndex = 0;
+
+        this.addTask = function (task) {
+
+            if (_self.myTask == "") {
+                alert("Plz fill input");
+            }
+            else {
+                _self.allTasks.push(task);
+                _self.allTaskLength = _self.allTasks.length;
+                _self.completedTaskLength++;
+                _self.myTask = "";
+            }
+
+        };
+
+        this.selectedTask = function (task) {
+
+            _self.removeIndex = _self.completedTask.indexOf(task);
+            if (_self.removeIndex > -1)
+                _self.completedTask.splice(_self.removeIndex, 1);
+
+            else
+                _self.completedTask.push(task);
+
+            _self.completedTaskLength = _self.allTaskLength -_self.completedTask.length;
+            console.log(_self.completedTaskLength);
+
+        };
 
 
-app.controller("toDoAppController", function () {
+        this.deleteTask = function () {
+          _self.completedTask = [];
 
-    var self = this;
 
-    this.myTask = "";
-    this.allTasks = [];
-    this.allTaskLength = 0;
-    this.completedTask = [];
-    this.completedTaskLength = 0;
-    this.removeIndex = 0;
 
-    this.addTask = function (task) {
-
-        if (this.myTask == "") {
-            alert("Plz fill input");
         }
-        else {
-            this.allTasks.push(task);
-            this.allTaskLength = this.allTasks.length;
-            this.myTask = "";
-        }
-
-    };
-
-    this.selectedTask = function (task) {
-
-        this.removeIndex = this.completedTask.indexOf(task);
-        if (this.removeIndex > -1)
-            this.completedTask.splice(this.removeIndex, 1);
-        else
-            this.completedTask.push(task);
-        this.completedTaskLength = this.completedTask.length;
-        console.log(this.completedTaskLength);
-
     }
-
-
-});
-app.config(function ($mdThemingProvider) {
-    $mdThemingProvider.theme('default')
-        .primaryPalette('blue')
-        .accentPalette('red');
-});
-
+})();
